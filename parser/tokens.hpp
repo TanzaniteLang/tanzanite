ba1cb043f38b782f45cae044c234d8051645bc8b
Fire-The-Fox <firethefox77@gmail.com>
2023-08-03 16:38:16 +0200

    more diverse tokens

--- parser/tokens.hpp
+++ parser/tokens.hpp
@@ -3,11 +3,76 @@
 namespace Tanzanite {
     enum class TokenTypes {
         Eof = -1,
+        Whitespace,
         Illegal,
+
+        // Identifier and literals
         Identifier,
-        Operator,
-        Keyword,
-        Delimiter,
-        Value
+        String,
+        Char,
+        Int,
+        Float,
+        Bool,
+        Nil,
+
+        // Statement
+        Assing,
+        If,
+        Else,
+        Elsif,
+        For,
+        While,
+        Unless,
+        Until,
+        Break,
+        Case,
+        When,
+        With,
+
+        // Operators
+        Plus,
+        Minus,
+        Asterisk,
+        Slash,
+        Modulo,
+        DoubleAsterisk,
+        DoubleSlash,
+        Bang,
+        Tilda,
+        LeftShift,
+        RightShift,
+        Ampersand,
+        Pipe,
+        Caret,
+        Equals,
+        NotEquals,
+        LessThan,
+        LessAndEqualThan,
+        GreaterThan,
+        GreatedAndEqualThan,
+        SpaceShip,
+        And,
+        Or,
+        PipeTo,
+
+        // Delimiters
+        Dot,
+        Comma,
+        Colon,
+        Semicolon,
+        LBracket,
+        RBracket,
+        LSquareBracket,
+        RSquareBracket,
+        LSquiglyBracket,
+        RSquiglyBracket,
+        Do,
+        End,
+
+        // Reserved keywords
+        Def,
+        Fun,
+        Return,
+        Sizeof,
     };
 }
